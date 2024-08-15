69fe763b4085a042a932b2391e54b146944383bd
Gumnaam Master <47783065+GumnaamMaster@users.noreply.github.com>
2024-08-15 15:08:19 +0530

    Add files via upload

---
+++ WEBMAPS/WEBMAP.py
@@ -0,0 +1,26 @@
+import folium
+import pandas
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elv=list(data["ELEV"])
+def color_producer(el):
+    if el < 1000:
+        return "green"
+    elif 1000 <= el < 3000:
+        return "orange"
+    else:
+        return "red"
+
+map=folium.Map(location=[28.62,77.20],zoom_start=6,tiles="Mapbox Bright")
+fgv=folium.FeatureGroup(name="Volcanoes")
+for lt,ln,el in zip(lat,lon,elv):
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],fill=True,fill_opacity=0.9,radius=5,popup=str(el)+" m",color=color_producer(el)))
+fgp=folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open("world.json",'r',encoding="utf-8-sig").read(),
+style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"]< 10000000
+else "orange" if 10000000 <=x["properties"]["POP2005"] < 20000000 else "red"}))
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("map1.html")
